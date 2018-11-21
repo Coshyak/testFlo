3f324f7f94ccb1e13dcea5f39b87360f72934414
Cengiz.Guertusgil <cengiz.gurtusgil@gmail.com>
2018-11-21 11:47:27 +0100

    test

---
+++ login/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'cengiz');
+   define('DB_PASSWORD', 'Jango12345@');
+   define('DB_DATABASE', 'crm');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
