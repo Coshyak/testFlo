c6a8df92e5abda44bbef979e1acebef2407f3703
Cengiz.Guertusgil <cengiz.gurtusgil@gmail.com>
2018-11-23 14:24:21 +0100

    test

---
+++ loginEasy/register.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Registrieren </title>
+</head>
+<body>
+
+<div>
+    <h2>Registrieren</h2>
+    <p>Bitte erstelle eine Account</p>
+    <form action="register.php" method="post">
+        <input type="text" name="username" placeholder="Benutzername">
+        <input type="text" name="password" placeholder="Passwort">
+        <input type="text" name="passwordConfirm" placeholder="Passwort bestätigen">
+        <br>
+        <button type="submit" name="submit" value="submit">Registrieren</button>
+        <br>
+    </form>
+</div>
+</body>
+</html>
